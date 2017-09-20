3c60a76f061887031a10cd657a7bc009e6c03d1a
Ani Sridhar <asridhar2015@gmail.com>
2017-09-19 23:42:47 -0400

    Core models (draft)  created for website

--- food_world/food_core/models.py
+++ food_world/food_core/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	first_name = models.CharField(max_length=128, help_text="enter First Name here")
+	last_name = models.CharField(max_length=128, help_text="enter Last Name here")
+	email = models.EmailField()
+	phone_number = models.CharField(max_length=128)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        pass
+
+class Cart(models.Model):
+	user = models.ForeignKey(models.User)
+	total_price = models.DecimalField()
+	num_items = models.IntegerField()
+    class Meta:
+        verbose_name = "Cart"
+        verbose_name_plural = "Carts"
+
+    def __str__(self):
+        pass
+    
+class Snack(models.Model):
+	carts = models.ManyToManyField(models.Cart) 
+	wishlists = models.ManyToManyField(models.Cart)
+	name = models.CharField(max_length=128, help_text="name of food item")
+	price = models.DecimalField()
+	description = models.TextField()
+	nutrition_info = models.TextField()
+
+
+    class Meta:
+        verbose_name = "Snack"
+        verbose_name_plural = "Snacks"
+
+    def __str__(self):
+        pass
+        
+class Wishlist(models.Model):
+	user = models.ForeignKey(models.User)
+	total_price = models.DecimalField()
+	num_items = models.IntegerField()
+	class Meta:
+        verbose_name = "Wishlist"
+        verbose_name_plural = "Wus"
+
+    def __str__(self):
+        pass
+
+    
+    
