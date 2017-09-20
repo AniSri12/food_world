3c4ca3c2685b9c6d2e5aa0d277ec1c374fed0c12
Ani Sridhar <asridhar2015@gmail.com>
2017-09-20 18:44:40 -0400

    Fixed errors with models and added food_core app to settings

--- food_world/food_core/models.py
+++ food_world/food_core/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
 class User(models.Model):
-	first_name = models.CharField(max_length=128, help_text="enter First Name here")
-	last_name = models.CharField(max_length=128, help_text="enter Last Name here")
-	email = models.EmailField()
-	phone_number = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=128, help_text="enter First Name here")
+    last_name = models.CharField(max_length=128, help_text="enter Last Name here")
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=128)
 
     class Meta:
         verbose_name = "User"
@@ -14,25 +16,40 @@ class User(models.Model):
     def __str__(self):
         pass
 
+
 class Cart(models.Model):
-	user = models.ForeignKey(models.User)
-	total_price = models.DecimalField()
-	num_items = models.IntegerField()
+    user = models.ForeignKey(User, related_name="cart")
+    total_price = models.DecimalField(max_digits=6, decimal_places=2)
+    num_items = models.IntegerField()
+
     class Meta:
         verbose_name = "Cart"
         verbose_name_plural = "Carts"
 
     def __str__(self):
         pass
-    
-class Snack(models.Model):
-	carts = models.ManyToManyField(models.Cart) 
-	wishlists = models.ManyToManyField(models.Cart)
-	name = models.CharField(max_length=128, help_text="name of food item")
-	price = models.DecimalField()
-	description = models.TextField()
-	nutrition_info = models.TextField()
 
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, related_name="wishlist")
+    total_price = models.DecimalField(max_digits=6, decimal_places=2)
+    num_items = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Wishlist"
+        verbose_name_plural = "Wus"
+
+    def __str__(self):
+        pass
+
+
+class Snack(models.Model):
+    carts = models.ManyToManyField(Cart, related_name='cart')
+    wishlists = models.ManyToManyField(Wishlist, related_name='wishlist')
+    name = models.CharField(max_length=128, help_text="name of food item")
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    nutrition_info = models.TextField()
 
     class Meta:
         verbose_name = "Snack"
@@ -40,17 +57,6 @@ class Snack(models.Model):
 
     def __str__(self):
         pass
-        
-class Wishlist(models.Model):
-	user = models.ForeignKey(models.User)
-	total_price = models.DecimalField()
-	num_items = models.IntegerField()
-	class Meta:
-        verbose_name = "Wishlist"
-        verbose_name_plural = "Wus"
 
-    def __str__(self):
-        pass
 
-    
-    
+
