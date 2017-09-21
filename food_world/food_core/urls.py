251b4869b51d566f99db02d50d89849f025a211a
Justin Barry <justin.barry97@gmail.com>
2017-09-21 20:15:03 +0000

    urls

---
+++ food_world/food_core/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^users/(?P<pk>\d+)', views.getUsers),
+    url(r'^snacks/(?P<pk>\d+)', views.getSnacks),
+    url(r'^carts/(?P<pk>\d+)', views.getCarts),
+    url(r'^wishlists/(?P<pk>\d+)', views.getWishlist),
+    url(r'^users/create', views.createUser),
+    url(r'^snacks/create', views.createSnack),
+    url(r'^carts/create', views.createCart),
+    url(r'^wishlists/create', views.createWishlist),
+]
