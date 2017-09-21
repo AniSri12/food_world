2133c0f13c1fc948eb1064ffa46ee92f6fea8ce6
Justin Barry <justin.barry97@gmail.com>
2017-09-21 19:32:12 +0000

    error handling / fix post

--- food_world/food_core/views.py
+++ food_world/food_core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+import json
 from .models import User, Snack, Wishlist, Cart
 from django.views.decorators.csrf import csrf_exempt
 
@@ -9,13 +10,16 @@ from django.views.decorators.csrf import csrf_exempt
 
 def getUsers(request, pk):
 	print(pk)
-	user = User.objects.get(pk = pk)
+	try:
+		user = User.objects.get(pk = pk)
+	except:
+		return JsonResponse({"Status Code": "404"})
 	first_name = user.first_name
 	last_name = user.last_name
 	email  = user.email
 	phone_number = user.phone_number
 
-	return JsonResponse({'First Name': first_name, "Last Name": last_name, "email": email, "Phone Number": phone_number})
+	return JsonResponse({"Status Code": "200", 'First Name': first_name, "Last Name": last_name, "email": email, "Phone Number": phone_number})
 
 def getSnacks(request, pk):
 	snack = models.Snack.objects.get(pk = pk)
@@ -23,7 +27,7 @@ def getSnacks(request, pk):
 	description = snack.description
 	price = snack.price
 	nutrition_info = snack.nutrition_info
-	return JsonResponse({"Name": name, "Description": description, "Price": price, "Nutrition Info": nutrition_info})
+	return JsonResponse({"Status Code": "200","Name": name, "Description": description, "Price": price, "Nutrition Info": nutrition_info})
 
 def getCarts(request):
 	snack = models.Snack.objects.get(pk = pk)
@@ -37,17 +41,25 @@ def createUser(request):
 	email  = request.POST.get("Email", "No Email Provided")
 	phone_number = request.POST.get("Phone Number", "No Phone Number Provided")
 	new_user = User(first_name = first_name, last_name = last_name, email = email, phone_number = phone_number)
-	new_user.save()
+	
+	try:
+		new_user.save()
+		return JsonResponse({"Status Code": "200"})
+	except:
+		return JsonResponse({"Status Code": "500"})
 
 @csrf_exempt
 def createSnack(request):
-	name = request.POST.get("Name", "No Name Provided")
-	description  = request.POST.get("Description", "No Description Provided")
-	price = request.POST.get("Price", 0.00)
-	nutrition_info = request.POST.get("Nutrition Info","No Nutrition Info Provided")
+	body_unicode = request.body.decode('utf-8')
+	body = json.loads(body_unicode)
+	name = body.get("Name", "No Name Provided")
+	description  = body.get("Description", "No Description Provided")
+	price = body.get("Price", 0.00)
+	nutrition_info = body.get("Nutrition Info","No Nutrition Info Provided")
 	new_snack = Snack(name = name, description = description, price = price, nutrition_info = nutrition_info)
+
 	try:
 		new_snack.save()
-		return JsonResponse({"Status Code": "200", "NameTest": request.POST})
+		return JsonResponse({"Status Code": "200"})
 	except:
 		return JsonResponse({"Status Code": "500"})
