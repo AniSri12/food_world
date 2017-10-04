36e5eaf674eb105cb2d4a3a3ce9e9a0012c43e69
Justin Barry <justin.barry97@gmail.com>
2017-10-04 15:07:14 -0400

    Home page added CSS and snack loading

--- food_world_web/food_world_web/views.py
+++ food_world_web/food_world_web/views.py
@@ -7,8 +7,8 @@ from django.http import JsonResponse
 
 
 def index(request):
-	req_home= urllib.request.Request('localhost:8002/api/v1/')
-	# resp_home= urllib.request.urlopen(req_home).read().decode('utf-8')
-	# resp = json.loads(resp_home)
-
-	return render(request, 'index.html')
+	req_home= urllib.request.Request('http://exp-api:8000/api/v1/')
+	resp_home= urllib.request.urlopen(req_home).read().decode('utf-8')
+	resp = json.loads(resp_home)
+	context = resp["Data"]
+	return render(request, 'index.html', {"Context" : context })
