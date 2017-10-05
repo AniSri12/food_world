ed04ccdeec27afe23c1faa731151784cc1953dd9
cavalier111 <rklemchek@yahoo.com>
2017-10-05 00:11:18 -0400

    fixed update and create, changed urls so that pattern matching doesnt match wrong url

--- food_world/food_core/urls.py
+++ food_world/food_core/urls.py
@@ -3,21 +3,23 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^users/(?P<pk>\d+)', views.getUsers, name = "get_user"),
-    url(r'^snacks/(?P<pk>\d+)', views.getSnacks, name = "get_snacks"),
-    url(r'^snacks/', views.get_all_snacks, name = "get_all_snacks"),
-    url(r'^users/', views.get_all_users, name = "get_all_users"),
+    
     url(r'^users/create', views.createUser, name = "create_user"),
     url(r'^snacks/create', views.createSnack, name = "create_snack"),
     url(r'^carts/create', views.createCart, name = "create_cart"),
     url(r'^wishlists/create', views.createWishlist, name = "create_wishlist"),
-    url(r'^users/destroy', views.destroyUser),
-    url(r'^snacks/destroy', views.destroySnack),
-    url(r'^carts/destroy', views.destroyCart),
-    url(r'^wishlists/destroy', views.destroyWishlist),
-    url(r'^users/update', views.updateUser),
-    url(r'^snacks/update', views.updateSnack),
-    url(r'^carts/update', views.updateCart),
-    url(r'^wishlists/update', views.updateWishlist),
+    url(r'^users/destroy/(?P<pk>\d+)', views.destroyUser),
+    url(r'^snacks/destroy/(?P<pk>\d+)', views.destroySnack),
+    url(r'^carts/destroy/(?P<pk>\d+)', views.destroyCart),
+    url(r'^wishlists/destroy/(?P<pk>\d+)', views.destroyWishlist),
+    url(r'^users/update/(?P<pk>\d+)', views.updateUser),
+    url(r'^snacks/update/(?P<pk>\d+)', views.updateSnack),
+    url(r'^carts/update/(?P<pk>\d+)', views.updateCart),
+    url(r'^wishlists/update/(?P<pk>\d+)', views.updateWishlist),
+    url(r'^snacks/', views.get_all_snacks, name = "get_all_snacks"),
+    url(r'^users/', views.get_all_users, name = "get_all_users"),
+    url(r'^users/(?P<pk>\d+)', views.getUsers, name = "get_user"),
+    url(r'^snacks/(?P<pk>\d+)', views.getSnacks, name = "get_snacks"),
+    
 
 ]
--- food_world/food_core/views.py
+++ food_world/food_core/views.py
@@ -10,28 +10,28 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-# def getCarts(request, pk):
-#   try:
-#       cart = Cart.objects.get(pk = pk)
-#   except:
-#       return JsonResponse({"status_code": "404"})
-#   user = cart.user
-#   total_price = cart.total_price
-#   num_items = cart.num_items
-#   return JsonResponse({"status_code": "200", "User": user, "total_price": total_price, "num_items": num_items})
+def getCarts(request, pk):
+    try:
+    cart = Cart.objects.get(pk = pk)
+    except:
+        return JsonResponse({"status_code": "404"})
+    user = cart.user
+    total_price = cart.total_price
+    num_items = cart.num_items
+    return JsonResponse({"status_code": "200", "User": user, "total_price": total_price, "num_items": num_items})
 
 
-# def getWishlist(request, pk):
+def getWishlist(request, pk):
 
-#   try:
-#       wishlist= Wishlist.objects.get(pk = pk)
-#   except:
-#       return JsonResponse({"status_code": "404"})
+    try:
+        wishlist= Wishlist.objects.get(pk = pk)
+    except:
+        return JsonResponse({"status_code": "404"})
 
-#   user = wishlist.user
-#   total_price = wishlist.total_price
-#   num_items = wishlist.num_items
-#   return JsonResponse({"status_code": "200","User": user, "total_price": total_price, "num_items": num_items})
+    user = wishlist.user
+    total_price = wishlist.total_price
+    num_items = wishlist.num_items
+    return JsonResponse({"status_code": "200","User": user, "total_price": total_price, "num_items": num_items})
 
 
 
@@ -67,8 +67,8 @@ def getUsers(request, pk):
             cart_items = ""
 
             
-        return JsonResponse({"status_code": "200", "Data" : {'first_name': first_name, "last_name": last_name, "email": email, "phone_number": phone_number, "Wishlist": {"total_price": wishlist_price, "num_items": wishlist_items}, "Cart": {"total_price": cart_price, "num_items": cart_items}}})
-
+        return JsonResponse({"status_code": "200", "data" : {'first_name': first_name, "last_name": last_name, "email": email, "phone_number": phone_number, "Wishlist": {"total_price": wishlist_price, "num_items": wishlist_items}, "Cart": {"total_price": cart_price, "num_items": cart_items}}})
+    return JsonResponse({"status_code": 500})
 
 
 def getSnacks(request, pk):
@@ -81,8 +81,8 @@ def getSnacks(request, pk):
         description = snack.description
         price = snack.price
         nutrition_info = snack.nutrition_info
-        return JsonResponse({"status_code": "200","Data" : {"name": name, "description": description, "price": price, "nutrition_info": nutrition_info}})
-
+        return JsonResponse({"status_code": "200","data" : {"name": name, "description": description, "price": price, "nutrition_info": nutrition_info}})
+    return JsonResponse({"status_code": 500})
 
 def get_all_snacks(request):
     if request.method == "GET":
@@ -101,8 +101,8 @@ def get_all_snacks(request):
 
             compiled_snack_data = {"pk" : snack.pk, "name": name, "description": description, "price": price, "nutrition_info": nutrition_info}
             all_snack_dict.append(compiled_snack_data)
-        return JsonResponse({"status_code": 200,"Data" : all_snack_dict})
-
+        return JsonResponse({"status_code": 200,"data" : all_snack_dict})
+    return JsonResponse({"status_code": 500})
 
 def get_all_users(request): #Only returns some user info for home screen
     if request.method == "GET":
@@ -122,9 +122,10 @@ def get_all_users(request): #Only returns some user info for home screen
 
             compiled_user_data = {'first_name': first_name, "last_name": last_name, "email": email, "phone_number": phone_number, "pk": pk}
             
-        all_users_dict.append(compiled_user_data)
+            all_users_dict.append(compiled_user_data)
             
-        return JsonResponse({"status_code": 200,"Data" : all_users_dict})
+        return JsonResponse({"status_code": 200,"data" : all_users_dict})
+    return JsonResponse({"status_code": 500})
 
 
 @csrf_exempt
@@ -195,8 +196,10 @@ def destroyUser(request,pk):
     try:
         user = User.objects.get(pk = pk)
         user.delete()
+        return JsonResponse({"status_code": "200", })
+
     except:
-        return JsonResponse({"status_code": "404"})
+        return JsonResponse({"status_code": "500"})
 
 @csrf_exempt
 def destroySnack(request,pk):
@@ -204,7 +207,7 @@ def destroySnack(request,pk):
         snack = Snack.objects.get(pk = pk)
         snack.delete()
     except:
-        return JsonResponse({"status_code": "404"})
+        return JsonResponse({"status_code": "500"})
 
 @csrf_exempt
 def destroyCart(request,pk):
@@ -212,7 +215,7 @@ def destroyCart(request,pk):
         cart = Cart.objects.get(pk = pk)
         cart.delete()
     except:
-        return JsonResponse({"status_code": "404"})
+        return JsonResponse({"status_code": "500"})
 
 @csrf_exempt
 def destroyWishlist(request,pk):
@@ -220,37 +223,41 @@ def destroyWishlist(request,pk):
         wishlist = Wishlist.objects.get(pk = pk)
         wishlist.delete()
     except:
-        return JsonResponse({"status_code": "404"})
+        return JsonResponse({"status_code": "500"})
 
 @csrf_exempt
 def updateUser(request,pk):
     try:
         user, created = User.objects.update_or_create(
-        pk=pk, defaults=request.body)
+        pk=pk, defaults=request.POST)
+        return JsonResponse({"status_code": "200", "data" : {'first_name': user.first_name, "last_name": user.last_name, "email": user.email, "phone_number": user.phone_number}})
     except:
-        return JsonResponse({"status_code": "404"})
+        return JsonResponse({"status_code": "500"})
 
 @csrf_exempt
 def updateSnack(request,pk):
     try:
         snack, created = Snack.objects.update_or_create(
-        pk=pk, defaults=request.body)
+        pk=pk, defaults=request.POST)
+        return JsonResponse({"status_code": "200","data" : {"name": name, "description": description, "price": price, "nutrition_info": nutrition_info}})
     except:
-        return JsonResponse({"status_code": "404"})
+        return JsonResponse({"status_code": "500"})
 
 @csrf_exempt
 def updateCart(request,pk):
     try:
         cart, created = Cart.objects.update_or_create(
-        pk=pk, defaults=request.body)
+        pk=pk, defaults=request.POST)
+        return JsonResponse({"status_code": "200", "User": user, "total_price": total_price, "num_items": num_items})
     except:
-        return JsonResponse({"status_code": "404"})
+        return JsonResponse({"status_code": "500"})
 
 @csrf_exempt
 def updateWishlist(request,pk):
     try:
         wishlist, created = Wishlist.objects.update_or_create(
-        pk=pk, defaults=request.body)
+        pk=pk, defaults=request.POST)
+        return JsonResponse({"status_code": "200","User": user, "total_price": total_price, "num_items": num_items})
     except:
-        return JsonResponse({"status_code": "404"})
+        return JsonResponse({"status_code": "500"})
 
