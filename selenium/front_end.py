0e1ce766f080088c35be3d82f55f836c22debe26
Ani Sridhar <asridhar2015@gmail.com>
2017-11-16 17:24:44 -0500

    Created selenium tests

--- selenium/front_end.py
+++ selenium/front_end.py
@@ -15,7 +15,138 @@ sign_up.click()
 print(sign_up)
 print(driver.current_url)
 
+class foodworldTest(unittest.TestCase):
 
+    def setUp(self):
+	    self.driver = webdriver.Remote(
+	       command_executor='http://selenium-chrome:4444/wd/hub',
+	       desired_capabilities=DesiredCapabilities.CHROME)
+	   
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_signin(self):
+    	driver.get("http://web:8000") # Start 
+		sign_up = self.driver.find_element_by_xpath('/html/body/nav/div/ul/li[4]/a')
+		sign_up.click()
+
+		first_name = self.driver.find_element_by_xpath('//*[@id="id_first_name"]')
+		first_name.send_keys("Chad")
+
+		last_name = self.driver.find_element_by_xpath('//*[@id="id_last_name"]')
+		last_name = self.send_keys("Mango")
+		email = self.driver.find_element_by_xpath('//*[@id="id_email"]')
+		email.send_keys("mango@chad.com")
+		password = self.driver.find_element_by_xpath('//*[@id="id_password"]')
+		password.send_keys("abc123")
+		phone_number = self.driver.find_element_by_xpath('//*[@id="id_phone_number"]')
+		phone_number.send_keys("123-456-6789")
+
+		sign_up_button = self.driver.find_element_by_xpath('//*[@id="id_phone_number"]')
+		sign_up_button.click()
+
+		email_login = self.driver.find_element_by_xpath('//*[@id="id_email"]')
+		email_login.send_keys("mango@chad.com")
+		pass_login = self.driver.find_element_by_xpath('//*[@id="id_password"]')
+		pass_login.send_keys("abc123")
+
+		login_butt = self.driver.find_element_by_xpath('/html/body/form/button[1]')
+		login_butt.click()
+
+		logout_button = self.driver.find_element_by_xpath('/html/body/nav/div/ul/li[5]/a')
+		self.assertEquals(logout_button.text, "Logout")
+
+	def test_addsnack(self):
+		driver.get("http://web:8000") # Start 
+		sign_up = self.driver.find_element_by_xpath('/html/body/nav/div/ul/li[4]/a')
+		sign_up.click()
+
+		first_name = self.driver.find_element_by_xpath('//*[@id="id_first_name"]')
+		first_name.send_keys("Chado")
+
+		last_name = self.driver.find_element_by_xpath('//*[@id="id_last_name"]')
+		last_name = self.send_keys("Mangoy")
+		email = self.driver.find_element_by_xpath('//*[@id="id_email"]')
+		email.send_keys("money@cnn.com")
+		password = self.driver.find_element_by_xpath('//*[@id="id_password"]')
+		password.send_keys("lalalala123")
+		phone_number = self.driver.find_element_by_xpath('//*[@id="id_phone_number"]')
+		phone_number.send_keys("123-456-6789")
+
+		sign_up_button = self.driver.find_element_by_xpath('//*[@id="id_phone_number"]')
+		sign_up_button.click()
+
+		email_login = self.driver.find_element_by_xpath('//*[@id="id_email"]')
+		email_login.send_keys("money@cnn.com")
+		pass_login = self.driver.find_element_by_xpath('//*[@id="id_password"]')
+		pass_login.send_keys("lalalala123")
+
+		login_butt = self.driver.find_element_by_xpath('/html/body/form/button[1]')
+		login_butt.click()
+
+		add_snack = self.drive.find_element_by_xpath("/html/body/nav/div/ul/li[3]/a")
+		add_snack.click()
+
+		name = self.driver.find_element_by_xpath('//*[@id="id_name"]')
+		name.send_keys("Apples")
+
+		nutrition_info = self.driver.find_element_by_xpath('//*[@id="id_nutrition_info"]')
+		nutrition_info.send_keys('Good for you')
+
+		country = self.driver.find_element_by_xpath('//*[@id="id_country"]')
+		country.send_keys("USA")
+
+		description = self.driver.find_element_by_xpath('//*[@id="id_description"]')
+		description.send_keys("Crisp apples")
+
+		price = self.driver.find_element_by_xpath('//*[@id="id_price"]')
+		price.send_keys('5.99')
+		
+
+		add_snacky = self.driver.find_element_by_xpath('/html/body/form/button')
+		add_snacky.click()
+
+		success_message = self.driver.find_element_by_xpath('/html/body/p[2]')
+
+		self.assertEquals(success_message.text, "Success! Your Snack Has Been Created!")
+
+
+
+
+
+
+	def test_register(self):
+		self.driver.get("http://web:8000") # Start 
+		sign_up = self.driver.find_element_by_xpath('/html/body/nav/div/ul/li[4]/a')
+		sign_up.click()
+
+		first_name = self.driver.find_element_by_xpath('//*[@id="id_first_name"]')
+		first_name.send_keys("Chad")
+
+		last_name = self.driver.find_element_by_xpath('//*[@id="id_last_name"]')
+		last_name = self.send_keys("Mango")
+		email = self.driver.find_element_by_xpath('//*[@id="id_email"]')
+		email.send_keys("chad@mango.com")
+		password = self.driver.find_element_by_xpath('//*[@id="id_password"]')
+		password.send_keys("123abc")
+		phone_number = self.driver.find_element_by_xpath('//*[@id="id_phone_number"]')
+		phone_number.send_keys("123-456-6789")
+
+		sign_up_button = self.driver.find_element_by_xpath('//*[@id="id_phone_number"]')
+		sign_up_button.click()
+
+		self.assertEqual(driver.currnel_url, "http://localhost:8000/login/")
+	def test_viewitem(self):
+		self.driver.get("http://web:8000")
+		tea = self.driver.find_element_by_xpath('/html/body/div[1]/a[2]/div/div[1]')
+		tea.click()
+		info = self.driver.find_element_by_xpath('/html/body/div[1]/div')
+		print(info.text)
+		self.assertEquals(info.text, Choice Organic Russian Caravan Black Tea)
+
+            
+    
 
 # class PythonOrgSearch(unittest.TestCase):
 
