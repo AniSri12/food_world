3c6221b0aaebe1ce26755c74b056cd709081f68f
cavalier111 <rklemchek@yahoo.com>
2017-12-03 17:51:28 -0500

    edited linking spark in docker file

--- batch_spark/message.py
+++ batch_spark/message.py
@@ -15,7 +15,7 @@ while not consumer:
 for message in consumer:
 	print(json.loads((message.value).decode('utf-8')))
 	print('HELLO')
-	some_new_rec= json.loads((message.value).decode('utf-8'))
+	some_new_rec = json.loads((message.value).decode('utf-8'))
 	user_pk = some_new_rec.get('user_pk', 'None')
 	item_pk = some_new_rec.get('item_pk', 'None')
 	log_file = open('log.txt', 'w')
