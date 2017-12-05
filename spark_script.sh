4531d8c176754e2bdfa4ee26e334dcabdba502a9
Ani Sridhar <asridhar2015@gmail.com>
2017-12-05 17:33:21 -0500

    fixed spark script

--- spark_script.sh
+++ spark_script.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-docker exec -it spark-master bash
+docker exec -it spark-master bash -c "
 apt-get update &&
 apt-get install python3-dev libmysqlclient-dev -y &&
 apt-get install python-pip -y &&
 pip install mysqlclient &&
-apt-get install python-mysqldb
-exit
+apt-get install python-mysqldb &&
+exit"
 
-docker exec -it spark-worker bash
+docker exec -it spark-worker bash -c "
 apt-get update &&
 apt-get install python3-dev libmysqlclient-dev -y &&
 apt-get install python-pip -y &&
 pip install mysqlclient &&
-apt-get install python-mysqldb
-exit
+apt-get install python-mysqldb &&
+exit"
