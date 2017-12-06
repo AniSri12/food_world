35a367da746aea05c45e756c21dd9aa1cf035d2d
cavalier111 <rklemchek@yahoo.com>
2017-12-06 00:14:31 -0500

    spark fixes

--- batch_spark/message.py
+++ batch_spark/message.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 import json
 print('running....')
 import os 
-dir_path = os.path.abspath("logs.log")
+dir_path = os.path.abspath("/app/food_world/batch-map-reduce/logs.log")
 
 
 es = None
@@ -17,10 +17,10 @@ while not consumer:
 
 for message in consumer:
 	print('FOUND!')
-	log = open(dir_path, 'a')
-	print(json.loads((message.value).decode('utf-8')))
-	some_new_snack = json.loads((message.value).decode('utf-8'))
-	user = some_new_snack['user_id']
-	item = some_new_snack['item_id']
-	log.write(str(user) + "	" + str(item))
-	log.close()
+	with open(dir_path, 'a') as log:
+		print(json.loads((message.value).decode('utf-8')))
+		some_new_snack = json.loads((message.value).decode('utf-8'))
+		user = some_new_snack['user_id']
+		item = some_new_snack['item_id']
+		log.write(str(user) + "	" + str(item))
+		log.write("\n")
