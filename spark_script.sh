0e24b1441d45ccd36a3f86144216519a830a15e3
Ani Sridhar <asridhar2015@gmail.com>
2017-12-06 02:33:45 -0500

    final spark fixes

--- spark_script.sh
+++ spark_script.sh
@@ -14,3 +14,12 @@ apt-get install python-pip -y &&
 pip install mysqlclient &&
 apt-get install python-mysqldb &&
 exit"
+
+
+
+
+while true
+do
+	docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-memory 512m /tmp/data/spark.py
+	sleep 120
+done
